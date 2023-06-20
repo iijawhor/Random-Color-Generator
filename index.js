034d1e2f2296832dc7eb944c8b3f8055aa5b7d51
Jawhor Ali Khan <jawhorali1@gmail.com>
2023-06-21 00:39:34 +0530

    Updated with arrow function

--- index.js
+++ index.js
@@ -2,17 +2,24 @@ let background = document.getElementById("bg");
 let button = document.getElementById("btn");
 let colorCode = document.getElementById("colorCode");
 // console.log(colorCode);
-let colours = [
-  "#ff9966",
-  "#631919",
-  "#3B185A",
-  "#656666",
-  "#008000",
-  "#000080",
-  "#008080",
-];
-button.addEventListener("click", function () {
-  let randomColor = (background.style.backgroundColor =
-    colours[Math.floor(Math.random() * colours.length)]);
-  colorCode.innerHTML = randomColor;
-});
+// let colours = [
+//   "#ff9966",
+//   "#631919",
+//   "#3B185A",
+//   "#656666",
+//   "#008000",
+//   "#000080",
+//   "#008080",
+// ];
+// button.addEventListener("click", function () {
+//   let randomColor = (background.style.backgroundColor =
+//     colours[Math.floor(Math.random() * colours.length)]);
+//   colorCode.innerHTML = randomColor;
+// });
+
+const backgroundColor = () => {
+  let randomColor = Math.floor(Math.random() * 1000);
+  background.style.backgroundColor = "#" + randomColor;
+  colorCode.innerHTML = "#" + randomColor;
+};
+button.addEventListener("click", backgroundColor);
